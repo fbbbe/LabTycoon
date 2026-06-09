c0734a24109dacd99ba75f34f8e81fcc871afae2
Kimnaae <kjillyt03@naver.com>
2026-06-09 18:21:10 +0900

    돈 효과음 추가

--- Assets/Scripts/UI/PopupManager.cs
+++ Assets/Scripts/UI/PopupManager.cs
@@ -6,9 +6,16 @@ public class PopupManager : MonoBehaviour
 
     public MoneyPopup moneyPopupPrefab;
 
+    [Header("효과음")]
+    public AudioClip coinSound;
+
+    private AudioSource audioSource;
+
     private void Awake()
     {
         Instance = this;
+
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     public void ShowMoneyPopup(Vector3 worldPosition, long amount)
@@ -25,5 +32,17 @@ public class PopupManager : MonoBehaviour
         popup.transform.position = screenPos;
 
         popup.Setup(amount);
+
+        PlayCoinSound();
+    }
+
+    private void PlayCoinSound()
+    {
+        if (coinSound == null)
+        {
+            return;
+        }
+
+        audioSource.PlayOneShot(coinSound);
     }
 }
